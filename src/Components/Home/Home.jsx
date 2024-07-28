192a197da13b885a6be423771a6ee31d24a008b7
Ruship2610 <rushipatel2610@gmail.com>
2024-07-28 15:33:49 +0530

    Final File

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -11,31 +11,11 @@ const Home = (props) => {
 
  
   return (
-    <>
-
-{/* <div className='sidebar'>
-        <ul>
-        <div onClick={() => {props.setCategory("entertainment")}} ><li  className="lists" >Entertainment</li></div>
-        <div onClick={() => {props.setCategory("sports")}} ><li className="lists" >Sports</li></div>
-        <div onClick={() => {props.setCategory("business")}}><li  className="lists" >Business</li></div>
-        <div onClick={() => {props.setCategory("science")}}><li className="lists" >Science</li></div>
-        <div onClick={() => {props.setCategory("technology")}}><li  className="lists" >Technology</li></div>
-        </ul>
-      
-    </div> */}
-    
-    
+    <> 
   <div className="content">
-
-   
- 
-   
      { props.products.map((news) => {
         return(
-        
-        
           <div  key={news.id}>
-        
           <div className="container card" >
             <div className="mainbox">
               <div className="img-box" >
@@ -47,8 +27,8 @@ const Home = (props) => {
              <p className='author'>Author : {news ? news.author : ""}</p>    
              <p id='read' ><a href={news ?news.readMoreUrl : ""} >Read More</a></p> 
            </div>
-  </div>
-  </div>  
+          </div>
+          </div>  
           </div>
          
         )
